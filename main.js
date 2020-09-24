bde28b86fd48b569160a0b52c8c6d03817577be0
Ismail-AbuSalah <ismail.i.abusalah@gmail.com>
2020-09-24 13:21:00 +0300

    add comments

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-const list = $(".list");
 
+// Selecting document elemnt using Jquery  
+const list = $(".list");
 const videoTitle = $(".title");
 
+// update the first video from array on load page.
+$("iframe").attr(`src`,`https://www.youtube.com/embed/${videos[0].id}`);
 videoTitle.text(`${videos[0].title}`);
 
-const frame = $("iframe")
-
+// Iterate over array to create a span for each video inculdes the img&title
 videos.forEach ((video) => {
     const more = $(`<span class='jj'>                 
         <img width=50% height=100% src=${video.thumbnail}><p>${video.title}</p></span>`);          
-    list.append(more);  
+    list.append(more);
+    
+    // on click ( update title & disply to none & src to new one)
     more.click( () => {
-        videoTitle.text(`${video.title}`);
-        more.css("display","none")
+    videoTitle.text(`${video.title}`);
+    more.css("display","none")
     })
     more.click( () => {
-        frame.attr(`src`,`https://www.youtube.com/embed/${video.id}`);
+        $("iframe").attr(`src`,`https://www.youtube.com/embed/${video.id}`);
     })
     
 });
