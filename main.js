1e6136ee1665d4826142c0eeef64435d40a7d38f
Ismail-AbuSalah <ismail.i.abusalah@gmail.com>
2020-09-24 11:35:03 +0300

    update everthing

---
+++ main.js
@@ -0,0 +1,19 @@
+const list = $(`.list`);
+
+const videoTitle = $(`.title`);
+
+videoTitle.text(`${videos[0].title}`);
+
+const frame = $("iframe")
+
+videos.forEach ((video) => {
+    const more = $(`<span class='jj'>                 
+        <img width=50% height=100% src=${video.thumbnail}><p>${video.title}</p></span>`);          
+    list.append(more);  
+    more.click( () => {
+        videoTitle.text(`${video.title}`);
+    })
+    more.click( () => {
+        frame.attr('src',`https://www.youtube.com/embed/${video.id}`);
+    })
+});
